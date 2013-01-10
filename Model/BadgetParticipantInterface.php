1ef98fab92ab955e6b269c9cae01453931e03655
ping86 <bmp_86@hotmail.com>
2013-01-10 13:54:21 +0100

    Add License

--- Model/BadgetParticipantInterface.php
+++ Model/BadgetParticipantInterface.php
@@ -1,5 +1,12 @@
 <?php 
-
+/**
+ * This file is part of the AntewesBadgeBundle package.
+ *
+ * (c) antweb <http://github.com/antwebes/>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 namespace antwebes\BadgeBundle;
 
 
